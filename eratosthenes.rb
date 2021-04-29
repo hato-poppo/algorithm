111ba4e99f508ae94a59e6451a3c03f9d7585ed7
hato-poppo <60541680+hato-poppo@users.noreply.github.com>
2021-04-29 22:26:34 +0900

    Update eratosthenes.rb

--- eratosthenes.rb
+++ eratosthenes.rb
@@ -1,9 +1,13 @@
 # エラトステネスの篩
+
+# 指定された値までに存在する素数を全て抽出するアルゴリズム
+
 # やっつけ実装3パターン
 
 max = 12000
 
-# 再代入しまくりバージョン
+# 実装パターン１
+# 派手な多重ループ＆再代入しまくりバージョン
 def eratosthenes０(numbers, max)
   sqrt = Math.sqrt(max)
   numbers.delete(1)
@@ -22,6 +26,9 @@ def eratosthenes０(numbers, max)
   prime + numbers
 end
 
+# 実装パターン２
+# パフォーマンスを意識してみた
+# 少しお淑やかな多重ループになったが、再代入は無くせず
 def eratosthenes1(nums, max)
   sqrt = Math.sqrt(max)
   nums.delete(1)
@@ -36,6 +43,8 @@ def eratosthenes1(nums, max)
   prime + nums
 end
 
+# 実装パターン３
+# パターン２からハッシュ化してみたけどあまり意味なくて結局変換のコストの方が大きそう
 def eratosthenes2(nums, max)
   sqrt = Math.sqrt(max)
   nums.delete(1)
@@ -55,18 +64,18 @@ end
 require 'benchmark'
 
 Benchmark.bm 10 do |r|
-  r.report "Nantoka" do
+  r.report "eratosthenes０" do
     eratosthenes０((1..max).to_a, max)
   end
-  r.report "Kantoka" do
+  r.report "eratosthenes１" do
     eratosthenes1((1..max).to_a, max)
   end
-  r.report "Kantoko" do
+  r.report "eratosthenes２" do
     eratosthenes2((1..max).to_a, max)
   end
 end
 
-#                  user     system      total        real
-# Nantoka      1.148480   0.000000   1.148480 (  1.148636)
-# Kantoka      0.006553   0.000000   0.006553 (  0.006554)
-# Kantoko      0.012244   0.000000   0.012244 (  0.012247)
+#                        user     system      total        real
+# eratosthenes０      1.148480   0.000000   1.148480 (  1.148636)
+# eratosthenes１      0.006553   0.000000   0.006553 (  0.006554)
+# eratosthenes２      0.012244   0.000000   0.012244 (  0.012247)
